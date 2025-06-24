66ebe7ad04dc5d51c9efe1e1afb2da40a35e3f96
urviKalal <133739755+urviKalal@users.noreply.github.com>
2025-06-24 13:29:25 +0530

    changes

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseUrl: 'https://shopy-production.up.railway.app',
     prepareHeaders: (headers) => {
       try {
         const userInfo = Cookies.get("admin");
